f9378015b8afad0dea46b3af366098da21d6b43b
Kenny <savorycyanide@gmail.com>
2016-10-25 18:03:21 -0700

    added stuff

--- phaser-template-gh-pages/game.js
+++ phaser-template-gh-pages/game.js
@@ -8,7 +8,7 @@ var mainState = {
     preload: function () {
         // This function will be executed at the beginning
         // That's where we load the game's assets
-        game.load.image('logo', 'missionbit.png');
+        game.load.spritesheet('bird', 'images/bird_sheet.png', 68, 48);
     },
     create: function () {
         // This function is called after the preload function
@@ -16,10 +16,12 @@ var mainState = {
 
         // Create a game sprite from the logo image positioned
         // at the center of the game world
-        this.sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        this.sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'bird');
         // The position of the sprite should be based on the
         // center of the image (default is top-left)
         this.sprite.anchor.setTo(0.5, 0.5);
+        this.sprite.animations.add('flap', [0,1,2,1], 10, true);
+        this.sprite.animations.play('flap');
         // Change background color to a gray color
         game.stage.backgroundColor = '#999999';
     },
@@ -29,6 +31,12 @@ var mainState = {
         
         // Rotate the sprite by 1 degrees
         this.sprite.angle += 1;
+        
+    game.physics.enable(this.sprite);
+    game.physics.arcade.gravity.y = 100;
+    // Stop the bird from falling off the screen, for now
+    this.sprite.body.collideWorldBounds = true;
+        
     }
 };
 
@@ -38,3 +46,7 @@ game = new Phaser.Game(640, 480, Phaser.AUTO, 'gameDiv');
 // And finally we tell Phaser to add and start our 'main' state
 game.state.add('main', mainState);
 game.state.start('main');
+
+// keep space from scrolling the page
+this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
